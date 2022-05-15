e0fea43e98f133d74eb1b0e4df37f37e9cbc8a73
David Michael II <knowdavidmichael@gmail.com>
2022-05-15 22:50:19 +0100

    feat: add custom theme to app

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { Provider as StyletronProvider } from 'styletron-react';
 import { BaseProvider } from 'baseui';
 import styletron from '../lib/styletron';
+import SynkTheme from '../lib/theme';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
     <StyletronProvider value={styletron}>
-      <BaseProvider>
+      <BaseProvider theme={SynkTheme}>
         <Component {...pageProps} />
       </BaseProvider>
     </StyletronProvider>
