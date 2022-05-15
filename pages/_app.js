2044482c6eb48f1e74d877348880d67e19c4b923
David Michael II <knowdavidmichael@gmail.com>
2022-05-16 00:02:48 +0100

    config: add apollo provider to app

--- pages/_app.js
+++ pages/_app.js
@@ -4,22 +4,28 @@ import { BaseProvider } from 'baseui';
 import { Provider } from 'react-redux';
 import { persistStore } from 'redux-persist';
 import { PersistGate } from 'redux-persist/integration/react';
+import { ApolloProvider } from '@apollo/client';
 import styletron from '../lib/styletron';
 import SynkTheme from '../lib/theme';
 import store from '../redux/store';
+import { useApollo } from '../lib/apollo';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
+  const { initialApolloState } = pageProps;
+  const client = useApollo(initialApolloState);
   const persistor = persistStore(store);
 
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <StyletronProvider value={styletron}>
-          <BaseProvider theme={SynkTheme}>
-            <Component {...pageProps} />
-          </BaseProvider>
-        </StyletronProvider>
+        <ApolloProvider client={client}>
+          <StyletronProvider value={styletron}>
+            <BaseProvider theme={SynkTheme}>
+              <Component {...pageProps} />
+            </BaseProvider>
+          </StyletronProvider>
+        </ApolloProvider>
       </PersistGate>
     </Provider>
   );
