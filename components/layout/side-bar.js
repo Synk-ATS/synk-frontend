5ef407ef4793406b28ff8d5f5a745762a7ff6334
David Michael II <knowdavidmichael@gmail.com>
2022-05-16 23:55:37 +0100

    feat: add side-bar list

--- components/layout/side-bar.js
+++ components/layout/side-bar.js
@@ -1,64 +1,147 @@
 import React from 'react';
-import { Navigation } from 'baseui/side-navigation';
+import { useStyletron } from 'baseui';
+import Link from 'next/link';
+import Image from 'next/image';
+import {
+  Checks, GearSix, Layout, Student, Users,
+} from 'phosphor-react';
+import { useRouter } from 'next/router';
+import { toggleIsExpanded, TreeView } from 'baseui/tree-view';
+
+function customLabel(node) {
+  const router = useRouter();
+  const [css, theme] = useStyletron();
+
+  return (
+    <div
+      role="button"
+      tabIndex={0}
+      onClick={() => router.push(node.id)}
+      onKeyDown={() => router.push(node.id)}
+      className={css({
+        ...theme.typography.ParagraphMedium,
+        color: theme.colors.mono200,
+        height: '100%',
+        width: '100%',
+      })}
+    >
+      <div
+        className={css({
+          outline: 'none',
+          backgroundColor: 'transparent',
+          display: 'flex',
+          justifyContent: 'flex-start',
+          alignItems: 'center',
+        })}
+      >
+        <div>{node.icon && node.icon}</div>
+        <p className={css({ marginLeft: '5px' })}>{node.title && node.title}</p>
+      </div>
+    </div>
+  );
+}
 
 const items = [
   {
+    id: '#dashboard',
+    title: 'Dashboard',
+    label: customLabel,
+    isExpanded: false,
+    icon: (<Layout />),
+  },
+  {
+    id: '#attendance',
     title: 'Attendance',
-    itemId: '#attendance',
-    subNav: [
-      {
-        title: 'Daily Attendance',
-        itemId: '#daily-attendance',
-      },
-      {
-        title: 'Monthly Report',
-        itemId: '#monthly-report',
-      },
-      {
-        title: 'Summary Report',
-        itemId: '#summary-report',
-      },
-    ],
+    label: customLabel,
+    icon: (<Checks />),
   },
   {
+    id: '#students',
     title: 'Students',
-    itemId: '#students',
-    subNav: [],
+    label: customLabel,
+    icon: (<Student />),
   },
   {
+    id: '#users',
     title: 'Users',
-    itemId: '#users',
-    subNav: [],
+    label: customLabel,
+    icon: (<Users />),
   },
   {
+    id: '#settings',
     title: 'Settings',
-    itemId: '#settings',
-    subNav: [],
+    label: customLabel,
+    icon: (<GearSix />),
   },
 ];
 
 function SideBar() {
-  const [activeItemId, setActiveItemId] = React.useState('#primary');
+  const [css, theme] = useStyletron();
+  const [data, setData] = React.useState(items);
   return (
-    <Navigation
-      items={items}
-      activeItemId={activeItemId}
-      onChange={({ item }) => setActiveItemId(item.itemId)}
-      overrides={{
-        Root: {
-          style: ({
-            height: '100%',
-            width: '250px',
-            backgroundColor: '#222222',
-            position: 'fixed',
-          }),
-        },
-        NavItem: {
-          style: ({
-          }),
-        },
-      }}
-    />
+    <nav className={css({
+      height: '100%',
+      top: 0,
+      width: '250px',
+      backgroundColor: theme.colors.primary,
+      position: 'fixed',
+      borderRightWidth: '3px',
+      borderRightStyle: 'solid',
+      borderRightColor: theme.colors.accent,
+    })}
+    >
+      <div>
+        <Link href="/" passHref>
+          <div className={css({
+            display: 'flex',
+            paddingLeft: '20px',
+            position: 'relative',
+            zIndex: 3,
+            maxHeight: '70px',
+            alignItems: 'center',
+            borderBottomWidth: '3px',
+            borderBottomStyle: 'solid',
+            borderBottomColor: theme.colors.accent,
+          })}
+          >
+            <Image
+              src="/logo-light.svg"
+              alt="Synk Logo"
+              height="80%"
+              width="80%"
+            />
+          </div>
+        </Link>
+      </div>
+      <div>
+        <TreeView
+          data={data}
+          onToggle={(node) => {
+            setData((prevData) => toggleIsExpanded(prevData, node));
+          }}
+          overrides={{
+            TreeLabel: {
+              style: ({ $isSelected }) => ({
+                paddingTop: '0.5rem',
+                paddingBottom: '0.5rem',
+                color: theme.colors.mono600,
+                cursor: 'pointer',
+                transitionProperty: 'all',
+                transitionDuration: theme.animation.timing600,
+                borderLeftWidth: '3px',
+                borderLeftStyle: 'solid',
+                borderLeftColor: $isSelected ? theme.colors.accent : 'transparent',
+                backgroundColor: $isSelected ? theme.colors.mono900 : 'transparent',
+                ':hover': {
+                  color: theme.colors.mono800,
+                  backgroundColor: theme.colors.mono900,
+                },
+              }),
+            },
+          }}
+        />
+      </div>
+    </nav>
 
   );
 }
