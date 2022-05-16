0afc576ee2dcebfa8deb4727c8c5c490f7cde43d
David Michael II <knowdavidmichael@gmail.com>
2022-05-16 10:26:44 +0100

    config: finish redux and apollo config

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,5 @@
+/* eslint-disable react/prop-types */
+
 import React from 'react';
 import { Provider as StyletronProvider } from 'styletron-react';
 import { BaseProvider } from 'baseui';
@@ -5,19 +7,21 @@ import { Provider } from 'react-redux';
 import { persistStore } from 'redux-persist';
 import { PersistGate } from 'redux-persist/integration/react';
 import { ApolloProvider } from '@apollo/client';
+import App from 'next/app';
 import styletron from '../lib/styletron';
 import SynkTheme from '../lib/theme';
-import store from '../redux/store';
+import synkStore from '../redux/store';
 import { useApollo } from '../lib/apollo';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  const { initialApolloState } = pageProps;
+  const { initialApolloState, initialReduxState } = pageProps;
   const client = useApollo(initialApolloState);
-  const persistor = persistStore(store);
+  const _store = synkStore(initialReduxState);
+  const persistor = persistStore(_store);
 
   return (
-    <Provider store={store}>
+    <Provider store={_store}>
       <PersistGate loading={null} persistor={persistor}>
         <ApolloProvider client={client}>
           <StyletronProvider value={styletron}>
@@ -33,6 +37,9 @@ function MyApp({ Component, pageProps }) {
 
 export default MyApp;
 
-// MyApp.getInitialProps = async (context) => {
-//   const appProps = await App.getInitialProps(context);
-// };
+MyApp.getInitialProps = async (context) => {
+  const appProps = await App.getInitialProps(context);
+  const _store = synkStore();
+
+  return { ...appProps, pageProps: { initialReduxState: _store.getState() } };
+};
