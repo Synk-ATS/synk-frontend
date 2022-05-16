0810571c7ce81fe5b6b706897616bb2b772e981e
David Michael II <knowdavidmichael@gmail.com>
2022-05-16 18:15:46 +0100

    feat: add header to layer

--- components/layout/index.js
+++ components/layout/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useStyletron } from 'baseui';
 import SideBar from './side-bar';
+import Header from '../organisms/header';
 
 function Layout({ children }) {
   const [css] = useStyletron();
 
   return (
     <>
+      <Header />
       <SideBar />
       <main className={css({ maxWidth: '1440px', margin: '0 auto 0 250px' })}>
         {children}
