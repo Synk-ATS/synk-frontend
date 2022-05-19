831a26a78872902774a4f4017c9e1d6301957812
David Michael II <knowdavidmichael@gmail.com>
2022-05-19 15:34:58 +0100

    config: add next-auth configurations

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,3 +1,66 @@
 import NextAuth from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import axios from 'axios';
 
-export default NextAuth({});
+const options = {
+  providers: [
+    CredentialsProvider({
+      name: 'Synk',
+      credentials: {
+        email: { label: 'Email', type: 'text' },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials) {
+        if ('email' in credentials && 'password' in credentials) {
+          const { data } = await axios.post(process.env.AUTH_API, {
+            identifier: credentials.email,
+            password: credentials.password,
+          });
+          const user = data;
+
+          if (user) {
+            return user;
+          }
+
+          return null;
+        }
+
+        return null;
+      },
+    }),
+  ],
+  session: { strategy: 'jwt' },
+  callbacks: {
+    async redirect({ url, baseUrl }) {
+      // Allows relative callback URLs
+      if (url.startsWith('/')) return `${baseUrl}${url}`;
+      // Allows callback URLs on the same origin
+      if (new URL(url).origin === baseUrl) return url;
+      return baseUrl;
+    },
+    async session({ session, token }) {
+      const _session = session;
+      _session.jwt = token?.jwt;
+      _session.user = token?.user;
+      await _session;
+      return _session;
+    },
+    async jwt({ token, user }) {
+      const isSignIn = !!user;
+      const _token = token;
+
+      if (isSignIn) {
+        _token.jwt = user?.jwt;
+        _token.user = user?.user;
+      }
+      await _token;
+      return _token;
+    },
+  },
+  pages: {
+    signIn: '/auth/signin',
+    error: '/auth/signin',
+  },
+};
+
+export default (req, res) => NextAuth(req, res, options);
