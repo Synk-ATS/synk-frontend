0997121531c6600df71300bc57e0dab1aba3b6d4
David Michael II <knowdavidmichael@gmail.com>
2022-05-19 15:46:43 +0100

    update _app

--- pages/_app.js
+++ pages/_app.js
@@ -45,6 +45,5 @@ export default MyApp;
 MyApp.getInitialProps = async (context) => {
   const appProps = await App.getInitialProps(context);
   const _store = synkStore();
-
   return { ...appProps, pageProps: { initialReduxState: _store.getState() } };
 };
