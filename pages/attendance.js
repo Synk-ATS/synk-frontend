160737c2aacf8d39d48de604c59dbd15b231c0ad
David Michael II <knowdavidmichael@gmail.com>
2022-05-27 16:00:22 +0100

    feat: add header and table to attendance page

--- pages/attendance.js
+++ pages/attendance.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import { Block } from 'baseui/block';
 import { HeadingMedium } from 'baseui/typography';
+import { Table } from 'baseui/table';
 import Layout from '../components/layout';
 
 function Attendance() {
   return (
-    <Block>
+    <Block paddingLeft={['20px', '20px', '40px', '40px']} paddingRight={['20px', '20px', '40px', '40px']}>
       <HeadingMedium>Class Attendance</HeadingMedium>
+      <Table
+        columns={['#', 'Name', 'Age', 'Address']}
+        data={[
+          [
+            '1',
+            'Sarah Brown',
+            31,
+            '100 Broadway St., New York City, New York',
+          ],
+          [
+            '2',
+            'Jane Smith',
+            32,
+            '100 Market St., San Francisco, California',
+          ],
+        ]}
+      />
     </Block>
   );
 }
