70c571db085eb4b2877500f95ed576c746b8481b
David Michael II <knowdavidmichael@gmail.com>
2022-05-27 18:10:43 +0100

    feat: add auth reducer to redux store

--- redux/store.js
+++ redux/store.js
@@ -10,13 +10,16 @@ import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import storage from 'redux-persist/lib/storage';
 import { globalSlice } from './slices/global.slice';
 import { attendanceSlice } from './slices/attendance.slice';
+import { authSlice } from './slices/auth.slice';
 
 const attendanceConfig = { key: 'attendance', version: 1, storage };
+const authConfig = { key: 'auth', version: 1, storage };
 
 const synkStore = (preloadedState) => configureStore({
   reducer: combineReducers({
     global: globalSlice.reducer,
     attendance: persistReducer(attendanceConfig, attendanceSlice.reducer),
+    auth: persistReducer(authConfig, authSlice.reducer),
   }),
   middleware: (getDefaultMiddleware) => getDefaultMiddleware({
     serializableCheck: {
