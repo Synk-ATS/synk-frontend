6ccd2f977c5be8be44eed844f5d5bc09e9e37732
David Michael II <knowdavidmichael@gmail.com>
2022-06-15 18:15:11 +0100

    feat: create student course page

---
+++ contents/courses/student-courses.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { HeadingXLarge } from 'baseui/typography';
+
+function StudentCourses() {
+  return (
+    <div>
+      <HeadingXLarge>Student Courses</HeadingXLarge>
+    </div>
+  );
+}
+
+export default StudentCourses;
