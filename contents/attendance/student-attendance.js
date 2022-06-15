a52a977b99d101d77832e25ab37a3a172d9205a2
David Michael II <knowdavidmichael@gmail.com>
2022-06-15 18:52:13 +0100

    fix: gql query in student-attendance

--- contents/attendance/student-attendance.js
+++ contents/attendance/student-attendance.js
@@ -24,7 +24,7 @@ const StudentsQuery = gql`
               id
               attributes {
                 title
-                course_code
+                code
                 description
                 faculties {
                   data {
