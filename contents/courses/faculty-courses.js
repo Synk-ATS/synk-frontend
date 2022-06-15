7c4bb3fd1c2ae0216050f29a76e87ee0dfadb78b
David Michael II <knowdavidmichael@gmail.com>
2022-06-15 18:19:43 +0100

    feat: add padding to faculty-courses

--- contents/courses/faculty-courses.js
+++ contents/courses/faculty-courses.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { HeadingXLarge } from 'baseui/typography';
+import { Block } from 'baseui/block';
 
 function FacultyCourses() {
   return (
-    <div>
+    <Block
+      paddingLeft={['20px', '20px', '40px', '40px']}
+      paddingRight={['20px', '20px', '40px', '40px']}
+    >
       <HeadingXLarge>Faculty Courses</HeadingXLarge>
-    </div>
+    </Block>
   );
 }
 
