4d616c5ccc2c4d852490bba4b043dff1b10d19ca
David Michael II <knowdavidmichael@gmail.com>
2022-06-15 18:20:01 +0100

    feat: add padding to student-courses

--- contents/courses/student-courses.js
+++ contents/courses/student-courses.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { HeadingXLarge } from 'baseui/typography';
+import { Block } from 'baseui/block';
 
 function StudentCourses() {
   return (
-    <div>
+    <Block
+      paddingLeft={['20px', '20px', '40px', '40px']}
+      paddingRight={['20px', '20px', '40px', '40px']}
+    >
       <HeadingXLarge>Student Courses</HeadingXLarge>
-    </div>
+    </Block>
   );
 }
 
