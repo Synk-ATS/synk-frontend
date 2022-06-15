8dfe49362263611a64ed51fd40facc08a72d2f33
David Michael II <knowdavidmichael@gmail.com>
2022-06-15 18:15:01 +0100

    feat: create faculty page

---
+++ contents/courses/faculty-courses.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { HeadingXLarge } from 'baseui/typography';
+
+function FacultyCourses() {
+  return (
+    <div>
+      <HeadingXLarge>Faculty Courses</HeadingXLarge>
+    </div>
+  );
+}
+
+export default FacultyCourses;
